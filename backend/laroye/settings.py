e66487becff2df1c04a0e290b60577f67b79d8d9
mohammedaminu <aminuolawalekan@gmail.com>
2022-12-16 18:48:43 +0100

    update

--- backend/laroye/settings.py
+++ backend/laroye/settings.py
@@ -63,7 +63,7 @@ CORS_ORIGIN_ALLOW_ALL=True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
-    "http://laroye.ai/",
+    "http://laroye.ai",
 ]
 
 ROOT_URLCONF = "laroye.urls"
